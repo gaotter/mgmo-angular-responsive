f6e960cfe75af0241c18eb4497907f23fbadef71
gaotter <gaotter@gmail.com>
2019-05-03 15:45:30 +0200

    2 files modified, 4 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { MgmoResponsiveTableComponent } from './responsive-table/responsive-table.component';
+import { MgmoResponsiveTableComponent } from './responsive-table/components/responsive-table.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MgmoResponsiveTableComponent ],
+  declarations: [ AppComponent, MgmoResponsiveTableComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
